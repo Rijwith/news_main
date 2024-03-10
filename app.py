b8c8e6a963071a91eee71a62a7d557627761c5a2
Rijwith Mamidi <143695270+Rijwith@users.noreply.github.com>
2024-03-10 14:23:44 +0530

    app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ scopes = ['https://www.googleapis.com/auth/userinfo.profile',
           'openid']
 
 # Redirect URI for the OAuth flow
-redirect_uri = 'http://127.0.0.1:5000/callback'
+redirect_uri = 'https://news-main.onrender.com/callback'
 
 # Create the OAuth flow object
 flow = Flow.from_client_secrets_file(client_secrets_file, scopes=scopes, redirect_uri=redirect_uri)
